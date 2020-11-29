f03260ac96e4212883f110b48c15177bc8044f30
Michel Rinck <mrinck@michelrinck.de>
2020-11-29 15:04:27 +0100

    changed port to 8088

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app))
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(8088);
 }
 bootstrap();
